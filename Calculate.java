61da13bd7ce6ff0ad18b404e08064acfd2b71dca
remvord <rem-den@yandex.ru>
2017-03-12 17:47:05 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,15 @@
+/**
+Класс реализует калькулятор
+*/
+
 public class Calculate {
 
 		public static void main(String[] arg) {
-			System.out.println("Calculate...");
+				System.out.println("Calculate...");
+				int first = Integer.valueOf(arg[0]);
+				int second = Integer.valueOf(arg[1]);
+				int summ = first + second;
+				System.out.println("Sum " + summ);
+
 		}
 }
